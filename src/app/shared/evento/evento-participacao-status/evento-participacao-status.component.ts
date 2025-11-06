eee846d8951d138dfe983c5e1af68846d82c870b
Noemi1 <calmeida.no@gmail.com>
2025-11-06 15:00:02 -0300

    FIX: git add .

---
+++ src/app/shared/evento/evento-participacao-status/evento-participacao-status.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-evento-participacao-status',
+  standalone: false,
+  templateUrl: './evento-participacao-status.component.html',
+  styleUrl: './evento-participacao-status.component.css'
+})
+export class EventoParticipacaoStatusComponent {
+
+}
